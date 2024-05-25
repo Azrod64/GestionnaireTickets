3dfa67b1e597fd68f177082177af8c8353d9c1ba
Romuald Moisan <121852193+Azrod64@users.noreply.github.com>
2024-05-25 22:14:34 +0200

    Revert "Backend"

--- src/main/java/com/projets/repository/IngenieurRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projets.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projets.model.Ingenieur;
-
-public interface IngenieurRepository extends JpaRepository<Ingenieur, Integer> {
-
-}
--- src/main/java/com/projets/repository/TechnicienRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projets.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projets.model.Technicien;
-
-public interface TechnicienRepository extends JpaRepository<Technicien, Integer>{
-
-}
