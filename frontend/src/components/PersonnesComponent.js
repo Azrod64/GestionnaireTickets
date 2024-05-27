8168a8d866c9fb01ff60971410528712c5bce7ca
FrirexD <frirex.go@gmail.com>
2024-05-27 09:57:32 +0200

    personnes table

--- frontend/src/components/PersonnesComponent.js
+++ frontend/src/components/PersonnesComponent.js
@@ -1,4 +1,4 @@
-import React,  { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import personnesService from '../services/PersonneService';
 import './PersonnesComponent.css';
 
@@ -6,173 +6,267 @@ const PersonnesComponent = () => {
     const [people, setPeople] = useState([]);
     const [menuOpen, setMenuOpen] = useState(false);
     const [error, setError] = useState(null);
+    const [editingPersonId, setEditingPersonId] = useState(null);
+    const [draftPerson, setDraftPerson] = useState({});
     const [formData, setFormData] = useState({
         email: '',
         mdp: '',
         nom: '',
         prenom: '',
-        role: 'ingenieur'
-      });
+        role: 'ingenieur',
+        qualifications: '',
+        projets: '',
+        competences: ''
+    });
 
-    //   useEffect(() => {
-    //     const fetchPeople = async () => {
-    //         try {
-    //             const fetchedPeople = await personnesService.getPeople();
-    //             setPeople(fetchedPeople);
-    //         } catch (err) {
-    //             setError(err.message);
-    //         }
-    //     };
+    useEffect(() => {
+        const fetchPeople = async () => {
+            try {
+                const fetchedPeople = await personnesService.getPersonnes();
+                setPeople(fetchedPeople);
+            } catch (err) {
+                setError(err.message);
+            }
+        };
 
-    //     fetchPeople();
-    // }, []);
+        fetchPeople();
+    }, []);
 
-      const handleChange = (e) => {
+    const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData({
-          ...formData,
-          [name]: value
+            ...formData,
+            [name]: value
         });
-      };
-    
-      const handleSubmit = (e) => {
+    };
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const newPerson = {
             email: formData.email,
             nom: formData.nom,
             prenom: formData.prenom,
-            role: formData.role
+            role: formData.role,
+            qualifications: formData.role === 'ingenieur' ? formData.qualifications : '',
+            projets: formData.role === 'ingenieur' ? formData.projets : '',
+            competences: formData.role === 'technicien' ? formData.competences : ''
         };
-        setPeople([...people, newPerson]);
 
-        setFormData({
-            email: '',
-            mdp: '',
-            nom: '',
-            prenom: '',
-            role: 'ingenieur'
-        });
-      };
-      const toggleMenu = () => {
+        try {
+            const createdPerson = await personnesService.createUser(newPerson, formData.role);
+            setPeople([...people, createdPerson]);
+            setFormData({
+                email: '',
+                mdp: '',
+                nom: '',
+                prenom: '',
+                role: 'ingenieur',
+                qualifications: '',
+                projets: '',
+                competences: ''
+            });
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
+    const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
+
+    const editPerson = (personId) => {
+        const personToEdit = people.find(person => person.idPersonne === personId);
+        setDraftPerson({ ...personToEdit });
+        setEditingPersonId(personId);
+    };
+
+    const saveChanges = async (personId) => {
+
+        setEditingPersonId(null);
+    };
+
+    const handleDeletePerson = async (personId) => {
+
+        setPeople(people.filter(person => person.idPersonne !== personId));
+    };
+
+    const handleDraftChange = (e) => {
+        const { name, value } = e.target;
+        setDraftPerson({
+            ...draftPerson,
+            [name]: value
+        });
+    };
+
     return (
-      <div>
-        <div className={`burger-menu ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-          <div className="burger-menu-line"></div>
-          <div className="burger-menu-line"></div>
-          <div className="burger-menu-line"></div>
+        <div>
+            <div className={`burger-menu ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <div className="burger-menu-line"></div>
+                <div className="burger-menu-line"></div>
+                <div className="burger-menu-line"></div>
+            </div>
+
+            <h1>Gestionnaire de Personnes</h1>
+
+            <div className={`menu-links ${menuOpen ? 'open' : ''}`}>
+                <a href="/">Gestionnaire de Ticket</a>
+                <a href="/Personnes">Gestionnaire de Personnes</a>
+            </div>
+
+            <form id="formContainer" onSubmit={handleSubmit}>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+                <label htmlFor="mdp">Mot de passe:</label>
+                <input
+                    type="password"
+                    id="mdp"
+                    name="mdp"
+                    value={formData.mdp}
+                    onChange={handleChange}
+                />
+                <label htmlFor="nom">Nom:</label>
+                <input
+                    type="text"
+                    id="nom"
+                    name="nom"
+                    value={formData.nom}
+                    onChange={handleChange}
+                />
+                <label htmlFor="prenom">Prénom:</label>
+                <input
+                    type="text"
+                    id="prenom"
+                    name="prenom"
+                    value={formData.prenom}
+                    onChange={handleChange}
+                />
+                <label htmlFor="role">Rôle:</label>
+                <select id="role" name="role" value={formData.role} onChange={handleChange}>
+                    <option value="ingenieur">Ingénieur</option>
+                    <option value="technicien">Technicien</option>
+                </select>
+
+                {formData.role === 'ingenieur' && (
+                    <>
+                        <label htmlFor="qualifications">Qualifications:</label>
+                        <input
+                            type="text"
+                            id="qualifications"
+                            name="qualifications"
+                            value={formData.qualifications}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="projets">Nombre de projets à réaliser:</label>
+                        <input
+                            type="number"
+                            id="projets"
+                            name="projets"
+                            value={formData.projets}
+                            onChange={handleChange}
+                        />
+                    </>
+                )}
+
+                {formData.role === 'technicien' && (
+                    <>
+                        <label htmlFor="competences">Compétences:</label>
+                        <input
+                            type="text"
+                            id="competences"
+                            name="competences"
+                            value={formData.competences}
+                            onChange={handleChange}
+                        />
+                    </>
+                )}
+
+                <button id="submit" type="submit">Soumettre</button>
+            </form>
+
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+
+            <div id='table'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Email</th>
+                            <th>Nom</th>
+                            <th>Prénom</th>
+                            <th id="icons">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {people.map((person) => (
+                            <tr key={person.idPersonne}>
+                                <td>{person.idPersonne}</td>
+                                <td>{editingPersonId === person.idPersonne ? (
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        value={draftPerson.email}
+                                        onChange={handleDraftChange}
+                                    />
+                                ) : (
+                                    person.email
+                                )}</td>
+                                <td>{editingPersonId === person.idPersonne ? (
+                                    <input
+                                        type="text"
+                                        name="nom"
+                                        value={draftPerson.nom}
+                                        onChange={handleDraftChange}
+                                    />
+                                ) : (
+                                    person.nom
+                                )}</td>
+                                <td>{editingPersonId === person.idPersonne ? (
+                                    <input
+                                        type="text"
+                                        name="prenom"
+                                        value={draftPerson.prenom}
+                                        onChange={handleDraftChange}
+                                    />
+                                ) : (
+                                    person.prenom
+                                )}</td>
+                                
+                                <td>
+                                    {editingPersonId === person.idPersonne ? (
+                                        <img
+                                            style={{ width: '1.3em', margin: '0 5px 0 0' }}
+                                            src={`${process.env.PUBLIC_URL}/images/validate.png`}
+                                            alt="modify"
+                                            onClick={() => saveChanges(person.idPersonne)}
+                                        />
+                                    ) : (
+                                        <img
+                                            style={{ width: '1.3em', margin: '0 5px 0 0' }}
+                                            src={`${process.env.PUBLIC_URL}/images/modify.png`}
+                                            alt="modify"
+                                            onClick={() => editPerson(person.idPersonne)}
+                                        />
+                                    )}
+                                    <img
+                                        style={{ width: '1.3em' }}
+                                        src={`${process.env.PUBLIC_URL}/images/trash.png`}
+                                        alt="remove"
+                                        onClick={() => handleDeletePerson(person.idPersonne)}
+                                    />
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-  
-        <h1>Gestionnaire de Personnes</h1>
-  
-        <div className={`menu-links ${menuOpen ? 'open' : ''}`}>
-          <a href="/">Gestionnaire de Ticket</a>
-          <a href="/Personnes">Gestionnaire de Personnes</a>
-        </div>
-  
-        <form id="formContainer" onSubmit={handleSubmit}>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-          <label htmlFor="mdp">Mot de passe:</label>
-          <input
-            type="password"
-            id="mdp"
-            name="mdp"
-            value={formData.mdp}
-            onChange={handleChange}
-          />
-          <label htmlFor="nom">Nom:</label>
-          <input
-            type="text"
-            id="nom"
-            name="nom"
-            value={formData.nom}
-            onChange={handleChange}
-          />
-          <label htmlFor="prenom">Prénom:</label>
-          <input
-            type="text"
-            id="prenom"
-            name="prenom"
-            value={formData.prenom}
-            onChange={handleChange}
-          />
-          <label htmlFor="role">Rôle:</label>
-          <select id="role" name="role" value={formData.role} onChange={handleChange}>
-            <option value="ingenieur">Ingénieur</option>
-            <option value="technicien">Technicien</option>
-          </select>
-  
-          {formData.role === 'ingenieur' && (
-            <>
-              <label htmlFor="qualifications">Qualifications:</label>
-              <input
-                type="text"
-                id="qualifications"
-                name="qualifications"
-                value={formData.qualifications}
-                onChange={handleChange}
-              />
-              <label htmlFor="projets">Nombre de projets à réaliser:</label>
-              <input
-                type="number"
-                id="projets"
-                name="projets"
-                value={formData.projets}
-                onChange={handleChange}
-              />
-            </>
-          )}
-  
-          {formData.role === 'technicien' && (
-            <>
-              <label htmlFor="competences">Compétences:</label>
-              <input
-                type="text"
-                id="competences"
-                name="competences"
-                value={formData.competences}
-                onChange={handleChange}
-              />
-            </>
-          )}
-  
-          <button id="submit" type="submit">Soumettre</button>
-        </form>
-      <div id='table'>
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Email</th>
-            <th>Nom</th>
-            <th>Prénom</th>
-            <th>Rôle</th>
-            <th id="icons">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {people.map((person) => (
-            <tr key={person.id_personne}>
-              <td>{person.email}</td>
-              <td>{person.nom}</td>
-              <td>{person.prenom}</td>
-              <td>{person.role}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-    </div>
-    
-  );
+    );
 };
 
 export default PersonnesComponent;
